f9e1c7594e37d492f04a945704e1d281320ae161
jatin-05dev <jatindev214@gmail.com>
2025-11-20 12:05:27 +0530

    first commit

---
+++ all_keys.py
@@ -0,0 +1,28 @@
+import keyword
+all_key=keyword.kwlist
+print(all_key)
+print(len(all_key))
+
+soft_key=keyword.softkwlist
+print(soft_key)
+print(len(soft_key))
+
+total_key=keyword.kwlist + keyword.softkwlist
+print(total_key)
+print(len(total_key))
+ 
+
+# from  keyword import  kwlist
+# from  keyword import  softkwlist
+# print(softkwlist)
+# print(kwlist)
+
+
+
+# print(keyword.kwlist)
+# print(keyword.softkwlist)
+
+# print(kwlist)
+
+
+
---
+++ forloop.py
@@ -0,0 +1,114 @@
+
+l=[1,2,3,4,5,6,78]
+l1=[]
+for i in l:
+    if(i%2==0):
+       l1.append(i)
+
+print(l1)
+
+# odd ki
+list
+l=[1,2,3,4,5,6,78]
+l1=[]
+for i in l:
+    if(i%2!=0):
+       l1.append(i)
+
+print(l1)
+
+
+# tuple
+l=[1,2,3,4,5,6,78]
+l1=[]
+for i in l:
+    if(i%2!=0):
+       l1.append(i)
+
+print(tuple(l1)) 
+
+dict
+
+d={'name':'jatin','age':5,'active':True}
+for i in d.keys():
+    print(i)
+for i in d.values(),d.keys():
+    print(i)
+print(d.items())
+# dic se ati he to do rkh 
+for i,k in d.items():
+    print(i,"=",k)
+
+for i,k in d.items():
+    print(i,k)
+
+# parten
+
+# simple
+for i in range(1,6):
+    print(i,end=" ")
+
+n=int(input("enter number: "))
+for i in range(1,n+1):
+    for i in range(1,n+1):
+        print(i,end=" ")
+    print()
+# pattern
+
+n=int(input("enter ani"))
+for l in range(1,n+1):
+    for i in range(1,l+1):
+        print(i*2-1,end=" ")
+    print()
+
+# odd even
+n=int(input("enter number: "))
+for j in range(1,n+1):
+    for i in range(1,j+1):
+        print(i*2-1,end=" ")
+    print()
+
+x=1
+for i in range(1,n+1):
+    for  j in range(1,i+1):
+        print(x,end=",")
+        x=x+2
+    print()
+1.,2,3
+4,5,67,
+
+# patrern 
+n=int(input("enyter a number : "))
+x=1
+for i in range(1,n+1):
+    for _ in range(1,i+1):
+        print(x,end=" ")
+        x=x+2
+    print()
+
+# alpha key
+x="k" 
+y=chr(ord(x)+1)
+print(y)
+n=int(input("enter char"))
+for i in range(1,n+1):
+    ch='a'
+    for _ in range(1,i+1):
+        print(ch,end=' ')
+        ch=chr(ord(ch)+1)
+    print()
+# nesxrt
+
+for i in range(1,n+1):
+    ch="a"
+    for _ in range(1,n+1):
+        print(ch,end=" ")
+        ch=chr(ord(ch)+1)
+    print()
+n=int(input("enter char"))
+for i in range(1,n+1):
+    ch='a'
+    for _ in range(1,i+1):
+        print(ch,end=' ')
+        ch=chr(ord(ch)+2)
+    print()
