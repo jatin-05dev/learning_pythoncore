22eaa6e1f180f8f0976ba5d717fe1ed8210c6868
jatin-05dev <jatindev214@gmail.com>
2025-11-23 23:37:02 +0530

    d

---
+++ fun.py
@@ -0,0 +1,67 @@
+# def show():
+#     print("hello")
+
+# show()
+# wap to print natural numbers
+# def printn(n):
+#     for _ in range(1,n+1):
+#         return "hello"
+
+
+# x=int(input("ente number"))
+
+# res=printn(x)
+# print(res)
+# print(res)
+# print(res)
+# # ____________________
+
+
+
+
+
+# def play():
+#     print("hello bro")
+
+# print(play())
+
+# def play():
+#     return "hello"
+
+# print(play())
+# x=play()
+# print(play())
+# print(x)
+# x=int(input("enter :"))
+# function ke praametre
+# nhi to error
+# def fac(x):
+    # fa=1
+    # for i in range(1,x+1):
+    #   fa=fa*i     
+    # return fa
+
+# print(fac(x))
+
+# ----------------
+# def printn(n):
+#     for i in range(1,n+1):
+#         print(i*2-1)
+#     return "hello"
+
+# x=int(input("ente number"))
+
+# print(printn(x))
+# ---
+
+# x=int(input("ente number"))
+# def printn(n):
+#     for i in range(2,n+1,2):
+#         print(i)
+#     return "hello"
+
+
+# print(printn(x))
+
+
+
