6372ac8009ffdbe60789d4a98bef872c07a2b7f6
jatin-05dev <jatindev214@gmail.com>
2025-11-26 19:10:38 +0530

    p

---
+++ inpuy.py
@@ -0,0 +1,27 @@
+#input 
+#x= input("enter a num")
+#print(x)
+#print(type(x))
+
+#y= input("enter a num")
+#print(y)
+#print(type(y))
+#z=input[]
+#print(z)
+#print(type(z))
+
+# s=eval(input("enter anumber"))
+# print(s,type(s))
+#s=eval(input(s))
+#print(s,type(s))
+
+# x="10"
+# y="paras"
+# z=eval('x+y')
+# print(type(eval('x+y')))
+# print(z)
+
+print(float(input("enter anumberv "))+float(input("enter number ")))
+
+
+
