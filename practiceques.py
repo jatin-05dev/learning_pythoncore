d551d456747c1dea8e4c0d405381b8d939f80956
jatin-05dev <jatindev214@gmail.com>
2025-11-26 19:07:35 +0530

    new

---
+++ practiceques.py
@@ -0,0 +1,303 @@
+# if statement 
+# n=int(input("enter any number"))
+# if n>0:
+    # print(f'given number {n} is positive')
+
+# print("thnak for positive")
+# else
+# if else
+# x=int(input("enter a number"))
+# if x>0:
+#     print(f'giver number {x} is positive ')
+# else:
+#     print(f'giver number {x} is negative')
+
+# print("thank you")         
+# if independnt else depent 
+# aur metho dme op
+# els if depent 
+# if elif el elif me phle wala
+# if elif else
+# x=int(input("enter a number"))
+# if x>0:
+    # print(f'giver number {x} is positive ')
+# elif x<0:
+    # print(f'giver number {x} is negative')
+# else:
+    # print("number is 0")
+# print("thank you")
+# x=int(input("entr number"))
+# if (x%2==0):
+#     print("given number is even")
+# else:
+#     print("not even")
+# x=int(input("enter a"))
+# y=int(input("enter b"))
+# if(x==y):
+#     print("x and y are eq")
+# else:
+#     print("not equal")
+# ==datatype
+# x=eval(input("enter"))
+# y=eval(input("enter"))
+# if(x==y):
+#     print("x and y are eq")
+# else:
+#     print("not equal")
+
+
+# q1
+
+# x=eval(input("enter your age : "))
+# if(0<x<18):
+    # print("enetr your not adult")
+# elif(1<x<61):
+    # print("your are adult")
+# elif(60<x):
+    # print("you are senior")
+# else:
+    # print("enter valid age")    
+# invalid 60  se upar me condidion ayigi
+# q2
+# n=int(input("enter :"))
+# i=1
+# while(i<=n):
+#     print(i)
+#     i=i+1
+# q3
+# n=eval(input("enter :"))
+# i=0
+# while(i<=n):
+#     if(i<=n-2):
+#         print(i,end=",")
+#     else:
+#         print(i)
+#     i=i+2
+# 10even nmber
+# q
+# n=int(input("enter :"))
+# i=1
+# sum=0
+# while(i<=n):
+#     sum=sum+i
+#     if(i<=(n-1)):
+#         print(i,end="+")
+#     else:
+#         print(i,end="=")
+#     i=i+1
+# print(sum)
+
+# n=7
+# i=1
+# sum=0
+# while(i<=n):
+#     sum=sum+i
+#     if(i<n):
+#       print(2*i,end="+")
+#     else:
+#        print(2*i,end="=")
+#     i=i+1
+# print(sum)           
+# fac
+# n=int(input("enter :"))
+# i=1
+# mul=1
+# while(i<=n):
+#     mul=mul*i
+#     if(i<=(n-1)):
+#         print(i,end="*")
+#     else:
+#         print(i,end="=")
+#     i=i+1
+# print(mul)    
+# neven number hori zontal sum mul
+# odd number hori zontal sum mul
+# 10 tk ke 
+# n=int(input("enter even :"))
+# i=1
+# mul=1
+# while(i<=n):
+#     if(i%2==0):
+#         mul=mul*i
+#         if(i<=(n-1)):
+#             print(i,end="*")
+#         else:
+#             print(i,end="=")
+#     i=i+1
+# print(mul)
+# 1 
+# n=int(input("enter num"))
+# i=1
+# while(i<=n):
+#     print(2*i,end=" ")
+#     i=i+1
+
+# n=int(input("enter num"))
+# i=1
+# while(i<=n):
+#      print((2*i-1),end=" ")
+#      i=i+1
+# ntk
+
+# n=int(input("enter num"))
+# i=1
+# while(i<=n):
+#      if(i%2==0):
+#           print(i)
+#      i=i+1
+
+# n=int(input("enter num"))
+# i=1
+# sum=0
+# while(i<=n):
+#      e=2*i
+#      if(i<=n-1):
+#       print(2*i,end="+")
+#      else:
+#         print(2*i,end="=") 
+#      sum=sum+e
+#      i=i+1
+# print(sum)
+# n=int(input("enter num"))
+# i=1
+# sum=0
+# count=0
+# while(i<=n):
+#      if(i%2==0):
+#           sum=sum+i
+#           if(i<n-1):
+#                print(i,end="+")
+#           else:
+#                print(i,end="=")
+        
+        
+#      i=i+1
+
+# print(sum)
+
+# n=5523564546583648654864586
+# c=0
+# while(n!=0):
+#     n%10
+#     c=c+1
+#     n=n//10
+
+
+# print(c)
+    
+
+# kam 
+
+# n=int(input("enter a number :"))
+# i=1 
+# while(i<=n):
+#     print(i)
+#     i=i+1
+
+# re
+# n=int(input("enter a number :"))
+# i=2 
+# s=0
+# while(i<=n):
+#     s=s+i
+#     if(i<=n-2):
+#      print(i,end="+")
+#     else:
+#      print(i,end="=")  
+#     i=i+2
+# print(s) 
+# 
+# n=int(input("enter a number :"))
+# i=2 
+# s=1
+# while(i<=n):
+#     s=s*i
+#     if(i<=n-2):
+#      print(i,end="*")
+#     else:
+#      print(i,end="=")  
+#     i=i+2
+# print(s)           
+
+# n=eval(input("enter a number :"))
+# i=1 
+# s=1
+# while(i<=n):
+#     s=s+i
+#     if(i<=n-2):
+#      print(i,end="*")
+#     else:
+#      print(i,end="=")  
+#     i=i+2
+# print(s)     
+
+# count digit 
+# cnt=0
+# while(n!=0):
+#     cnt=cnt+1
+#     n = n//10
+
+    
+# print(cnt)
+
+# armsterongn=y=
+# x=y=n
+# td=0
+# ld=0
+# s=0
+# while(x>0):
+#     td=td+1
+#     x= x//10
+# while(y>0):
+#     ld=y%10
+#     s = s+ld**td
+#     y=y//10
+
+# if(n==s):
+#     print("arm strong")
+# else:
+#     print("not armstrong")
+# 0# 1se9 sb arm
+# n=153
+# x=n
+# sum=0
+# rem=0
+# cnt=0
+# c=3
+# while(x!=0):
+#     rem=x%10
+    
+#     sum=sum+rem**3
+#     x=x//10
+
+
+# print(sum)
+
+    
+
+
+# x=n
+# ld=0
+# s=0
+# while(x>0):
+#     ld=x%10
+#     s=s+(ld*ld*ld)
+#     x=x//10
+
+
+# if(n==s):
+#     print("arm strong")
+# else:
+#     print("not armstrong")
+
+
+     
+# 0# 1se9 sb arm
+
+# n=input("enter sny nunber")
+
+# if(n==n[::-1]):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+# print(type(n))
