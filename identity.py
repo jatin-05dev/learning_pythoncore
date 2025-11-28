675d63b05f2d26b6d264224ef10c079b0b41bdfa
jatin-05dev <jatindev214@gmail.com>
2025-11-28 23:43:34 +0530

    f

---
+++ identity.py
@@ -0,0 +1,20 @@
+x=10
+y="15"
+print(id(x),id(y))
+print(x is y)
+
+x1=[1,2,3,4]
+y1=[1,2,3,4]
+print(id(x1),id(y1))
+print(x is y)
+# 
+x2={'jatin':'neeraj'}
+y2={'jatin':'neeraj'}
+print(id(x2),id(y2))
+print(x2 is not y2)
+  
+    
+    
+    
+    
+
