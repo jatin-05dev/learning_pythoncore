35e06f000f22f0231ec6772f6619ad62035f6004
jatin-05dev <jatindev214@gmail.com>
2026-02-05 23:10:01 +0530

    practice

---
+++ index.py
@@ -0,0 +1,10 @@
+# index
+# num=[1,3,4]
+# print(num.index(2))
+# arr=[1,2,3,4,5,6,7,8,9,10]
+# print(arr.index(3,1))
+# range
+# ran=range(1,10,2)
+# print(tuple(ran))
+ran=range(-2,-10,-2)
+print(tuple(ran))
